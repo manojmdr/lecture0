fac2be6991e009d7e89a54161d07c11cd1a1cb85
Manoj <manoj@Manoj-Lenovo.localdomain>
2020-06-24 17:58:57 -0500

    added all files

---
+++ script.js
@@ -0,0 +1,12 @@
+function sayHi(params) {
+    document.write("I am a script");
+    if(a>b){
+        console.log("NOt true");
+        else{
+            console.log)"true";
+        }
+    }
+}
+sayHi();
+var a=4;
+var b=5;
